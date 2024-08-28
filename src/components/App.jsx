d10e66a19a631a8b227d96e709a815823b4a0917
louka <22691132+loukamb@users.noreply.github.com>
2024-08-28 11:36:16 -0400

    Scroll persistence, new theme, width adjust

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,16 +27,18 @@ function Tab({ onClick, onDelete, selected, children }) {
   return (
     <div
       onClick={onClick}
-      className={`w-fit inline-block px-2 py-2 select-none ${
-        selected ? "bg-slate-600" : ""
+      className={`w-fit group inline-flex items-center gap-1 px-2 py-2 select-none border-y-2 border-transparent ${
+        selected ? "border-b-sky-700" : "hover:border-b-sky-700/30"
       }`}
     >
       {children}
       <button
-        className="inline-flex items-center justify-center rounded-full bg-slate-800/50 w-5 h-5 ml-1 text-xs hover:bg-slate-800"
+        className={`inline-flex items-center justify-center rounded-full bg-zinc-100 w-4 h-4 text-xs hover:bg-zinc-200 hover:opacity-100 ${
+          selected ? "opacity-100" : "opacity-0 group-hover:opacity-100"
+        }`}
         onClick={(e) => (e.stopPropagation(), onDelete())}
       >
-        <Icon icon="fluent:delete-12-regular" />
+        <Icon icon="fluent:dismiss-12-regular" />
       </button>
     </div>
   )
@@ -64,27 +66,49 @@ function App() {
 
   const [bookSearch, setBookSearch] = useState("")
   const [verseSearch, setVerseSearch] = useState("")
-  const [zoomLevel, setZoomLevel] = useState(1)
   const [barHidden, setBarHidden] = useState(true)
-  const scriptureSelector = useRef(null)
+  const scriptureReader = useRef(null)
+
+  // NOTE: This is only a one-way bind, used for
+  // state persistence
+  const [scrollLevel, setScrollLevel] = useState(0)
+  const [zoomLevel, setZoomLevel] = useState(1)
+  const [verseWidth, setVerseWidth] = useState(1)
 
   const [selectedScripture, setSelectedScripture_internal] = useState("nkjv")
   const setSelectedScripture = useCallback((id) => {
     setSelectedScripture_internal(id)
     scriptures.load(id)
   })
-  useEffect(() => {
-    scriptures.load("nkjv")
-  }, [])
 
   const [readyToUpdateState, setReadyToUpdateState] = useState(false)
 
   // Useful functions
   // ====================================================================
 
-  /**
-   * Add a new tab.
-   */
+  /** Compress a tab */
+  function compressTab(tab) {
+    return [
+      tab.scripture.id,
+      tab.bookIndex,
+      tab.chapter.num - 1,
+      tab.scroll > 0 ? tab.scroll : undefined, // Optional
+    ]
+  }
+
+  /** Decompress a tab */
+  async function decompressTab(tab) {
+    const scripture = await scriptures.load(tab[0])
+    return {
+      scripture,
+      bookIndex: tab[1],
+      book: scripture.books[tab[1]],
+      chapter: scripture.books[tab[1]].chapters[tab[2]],
+      scroll: tab[4] ?? 0,
+    }
+  }
+
+  /** Add a tab */
   const addTab = useCallback(
     (scripture, book, chapter, focus) => {
       const existingTabIndex = tabs.findIndex(
@@ -100,8 +124,8 @@ function App() {
           book,
           chapter,
           scripture,
-          id: book.id,
-          index: scripture.books.findIndex((bk) => bk.name === book.name),
+          bookIndex: scripture.books.findIndex((bk) => bk.name === book.name),
+          scroll: 0,
         },
       ])
       if (focus) {
@@ -111,9 +135,7 @@ function App() {
     [tabs, selectedTabIndex]
   )
 
-  /**
-   * Delete a tab.
-   */
+  /** Delete a tab */
   const removeTab = useCallback(
     (tab) => {
       const index = tabs.findIndex((t) => t === tab)
@@ -139,20 +161,11 @@ function App() {
       const [stateIndex, indices] = JSON.parse(
         atob(decodeURIComponent(encodedState))
       )
-
       ;(async () => {
         const tabs = []
         for (const tab of indices) {
-          const scripture = await scriptures.load(tab[0])
-          tabs.push({
-            scripture,
-            index: tab[1],
-            book: scripture.books[tab[1]],
-            chapter: scripture.books[tab[1]].chapters[tab[2]],
-            id: scripture.books[tab[1]].id,
-          })
+          tabs.push(await decompressTab(tab))
         }
-
         setTabs(tabs)
         setSelectedTabIndex(stateIndex)
         setSelectedScripture(indices[stateIndex][0])
@@ -173,16 +186,7 @@ function App() {
         url.searchParams.set(
           "state",
           encodeURIComponent(
-            btoa(
-              JSON.stringify([
-                selectedTabIndex,
-                tabs.map((tab) => [
-                  tab.scripture.id,
-                  tab.index,
-                  tab.chapter.num - 1,
-                ]),
-              ])
-            )
+            btoa(JSON.stringify([selectedTabIndex, tabs.map(compressTab)]))
           )
         )
       } else {
@@ -190,7 +194,47 @@ function App() {
       }
       history.replaceState(null, "", url)
     }
-  }, [readyToUpdateState, tabs, selectedTabIndex])
+  }, [readyToUpdateState, tabs, selectedTabIndex, scrollLevel])
+
+  // Per-tab scroll persistence.
+  useEffect(() => {
+    const onScroll = (e) => {
+      // HACK: Setters shouldn't be used to get current values,
+      // but this works specifically for our purposes lol
+      setSelectedTabIndex((index) => {
+        setTabs((tabs) => {
+          tabs[index].scroll = e.target.scrollTop
+          setScrollLevel((previous) => {
+            if (e.target.scrollTop % 10 === 0) {
+              return e.target.scrollTop
+            }
+            return previous
+          })
+          return tabs
+        })
+        return index
+      })
+    }
+    if (scriptureReader.current != null) {
+      scriptureReader.current.addEventListener("scroll", onScroll)
+      return () =>
+        scriptureReader.current.removeEventListener("scroll", onScroll)
+    }
+  }, [scriptureReader.current])
+
+  // Updated scroll on tab selection
+  useEffect(() => {
+    if (tabs[selectedTabIndex] !== undefined) {
+      if (scriptureReader.current != null) {
+        scriptureReader.current.scrollTop = selectedTab.scroll
+      }
+    }
+  }, [tabs, selectedTabIndex, scriptureReader.current])
+
+  // Load NKJV by default
+  useEffect(() => {
+    scriptures.load("nkjv")
+  }, [])
 
   // Actual component lol
   // ====================================================================
@@ -202,7 +246,7 @@ function App() {
           className={`absolute left-0 top-0 w-screen h-screen z-40 transition duration-300 ${
             barHidden
               ? "pointer-events-none backdrop-blur-0 bg-transparent"
-              : "pointer-events-auto backdrop-blur-sm bg-black/20"
+              : "pointer-events-auto backdrop-blur-sm "
           }`}
           onClick={() => setBarHidden(true)}
         />
@@ -210,13 +254,12 @@ function App() {
         <div
           className={`${
             barHidden ? "-translate-x-full" : "translate-x-0"
-          } duration-300 lg:w-[24rem] lg:min-w-[24rem] w-[80%] min-w-[80%] absolute z-50 transition h-full overflow-y-scroll bg-slate-700 text-slate-50 scrollbar-thin scrollbar-thumb-slate-500 scrollbar-track-transparent scrollbar-corner-transparent`}
+          } duration-300 lg:w-[24rem] lg:min-w-[24rem] w-[80%] min-w-[80%] absolute z-50 transition h-full overflow-y-scroll bg-zinc-100 text-zinc-950 scrollbar-thin scrollbar-thumb-zinc-500 scrollbar-track-transparent scrollbar-corner-transparent`}
         >
-          <div className="flex items-center border-b group border-slate-500 focus:bg-slate-600 hover:bg-slate-600 px-4 py-2 gap-2 transition">
+          <div className="flex items-center border-b group border-zinc-300 focus:bg-zinc-200 hover:bg-zinc-200 px-4 py-2 gap-2 transition">
             <Icon icon="fluent:book-16-regular" />
             <select
-              ref={scriptureSelector}
-              className="w-full bg-slate-700 group-hover:bg-inherit placeholder-slate-500 outline-none font-sans"
+              className="w-full bg-zinc-100 group-hover:bg-inherit placeholder-zinc-500 outline-none font-sans"
               value={selectedScripture}
               onChange={(e) => setSelectedScripture(e.target.value)}
             >
@@ -238,7 +281,7 @@ function App() {
             value={bookSearch}
             onChange={(e) => setBookSearch(e.target.value.trim().toLowerCase())}
             placeholder="Search"
-            className="w-full bg-inherit placeholder-slate-500 focus:bg-slate-600 hover:bg-slate-600 border-b border-slate-500 outline-none font-sans p-4 transition"
+            className="w-full bg-inherit placeholder-zinc-500 focus:bg-zinc-200 hover:bg-zinc-200 border-b border-zinc-300 outline-none font-sans p-4 transition"
           />
           <div>
             {scriptures[selectedScripture] ? (
@@ -277,9 +320,9 @@ function App() {
           </div>
         </div>
         <main className="flex flex-col h-full w-0 grow">
-          <nav className="flex items-center bg-slate-800 h-10">
+          <nav className="flex items-center bg-zinc-100 shadow-lg h-10">
             <button
-              className="w-fit inline-block px-4 py-3 hover:bg-slate-600 cursor-default transition"
+              className="w-fit inline-block px-4 py-3 hover:bg-zinc-300 cursor-default transition"
               onClick={() => setBarHidden((c) => !c)}
             >
               <Icon icon="fluent:book-16-regular" />
@@ -299,25 +342,42 @@ function App() {
                   onDelete={() => removeTab(tab)}
                   selected={selectedTabIndex === i}
                 >
-                  <span className="text-xs bg-slate-900 px-1 rounded-md text-white/80">
+                  <span className="text-xs bg-zinc-100 px-0.5 rounded-md text-zinc-950 shadow-md border-2 border-zinc-400">
                     {tab.scripture.id.toUpperCase()}
                   </span>{" "}
                   {tab.book.name} {tab.chapter.num}
                 </Tab>
               ))}
             </div>
-            <div className="md:flex hidden items-center ml-auto p-4">
-              <Icon icon="fluent:search-12-regular" className="mr-2" />
-              <input
-                type="range"
-                min={1}
-                max={100}
-                value={zoomLevel}
-                onChange={(e) => setZoomLevel(e.target.valueAsNumber)}
-              />
+            <div className="md:flex gap-4 hidden ml-auto p-4">
+              <div className="flex items-center">
+                <Icon icon="fluent:search-12-regular" className="mr-2" />
+                <input
+                  type="range"
+                  min={1}
+                  max={100}
+                  value={zoomLevel}
+                  onChange={(e) => setZoomLevel(e.target.valueAsNumber)}
+                />
+              </div>
+              <div className="flex items-center">
+                <Icon icon="fluent:auto-fit-width-24-filled" className="mr-2" />
+                <input
+                  type="range"
+                  min={1}
+                  max={100}
+                  value={verseWidth}
+                  onChange={(e) => setVerseWidth(e.target.valueAsNumber)}
+                  className="w-16"
+                />
+              </div>
             </div>
           </nav>
-          <div className="reader" style={{ zoom: `${zoomLevel + 100}%` }}>
+          <div
+            ref={scriptureReader}
+            className="reader"
+            style={{ zoom: `${zoomLevel + 100}%` }}
+          >
             <div className="mx-auto">
               {selectedTab !== undefined && (
                 <>
@@ -330,7 +390,7 @@ function App() {
                       setVerseSearch(e.target.value.trim().toLowerCase())
                     }
                     placeholder="Search"
-                    className="sticky backdrop-blur-lg bg-slate-950/50 shadow-lg -top-12 w-full z-30 placeholder-slate-700 focus:bg-slate-800 hover:bg-slate-900 border border-slate-700 outline-none font-sans rounded-lg p-2 text-xl mb-8"
+                    className="sticky backdrop-blur-lg bg-white/50 -top-12 w-full z-30 placeholder-zinc-400 focus:bg-zinc-100/50 border border-zinc-300 hover:shadow-md outline-none font-sans rounded-lg p-2 text-xl mb-8"
                   />
                   {selectedTab.chapter.verses.map(
                     (verse) =>
@@ -339,7 +399,12 @@ function App() {
                         <Verse
                           key={verse.num}
                           num={verse.num}
-                          href={`https://bible.louka.sh/${selectedTab.id}/${selectedTab.chapter.num}/${verse.num}`}
+                          width={
+                            verseWidth === 100
+                              ? "100%"
+                              : `${verseWidth / 2 + 42}rem`
+                          }
+                          href={`https://bible.louka.sh/${selectedTab.book.id}/${selectedTab.chapter.num}/${verse.num}`}
                         >
                           {verse.text}
                         </Verse>
@@ -349,7 +414,7 @@ function App() {
               )}
             </div>
           </div>
-          <div className="flex flex-col md:flex-row text-xs items-center mt-auto bg-slate-800 px-4 py-2 md:text-sm gap-1">
+          <div className="flex flex-col md:flex-row text-xs items-center mt-auto bg-zinc-100 px-4 py-2 md:text-sm gap-1">
             Louka's Bible Explorer
             <span>
               (
--- src/components/Verse.jsx
+++ src/components/Verse.jsx
@@ -1,6 +1,10 @@
-export default function Verse({ id, num, children, subchildren, href }) {
+export default function Verse({ id, num, children, subchildren, href, width }) {
   return (
-    <div key={num} className="verse">
+    <div
+      key={num}
+      className="verse"
+      style={{ "--verse-width": width ?? "42rem" }}
+    >
       <div>
         <a id={num} className="ref" target="_blank" href={href}>
           <sup>{num}</sup>
